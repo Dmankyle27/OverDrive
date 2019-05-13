6c2733a6008ffc8d7fafa6801ce10091ae5ae56a
Logan-Purcell <39350014+Logan-Purcell@users.noreply.github.com>
2019-05-13 19:00:53 -0400

    Directorys

--- Sounds.py
+++ Sounds.py
@@ -2,22 +2,22 @@ import vlc
 import time
 
 def Engage_Sound():
-	sound_file = vlc.MediaPlayer("/home/logan/Desktop/OpenDrive/Electronic_Chime-KevanGC-495939803.mp3")
+	sound_file = vlc.MediaPlayer("Directory to electroinc chime")
 	sound_file.play()
 	time.sleep(2)
-	sound_file = vlc.MediaPlayer("/home/logan/Desktop/OpenDrive/engage.mp3")
+	sound_file = vlc.MediaPlayer("Directory to engage.mp3")
 	sound_file.play()
 
 
 def DisEngage_Sound():
-	sound_file = vlc.MediaPlayer("/home/logan/Desktop/OpenDrive/Electronic_Chime-KevanGC-495939803.mp3")
+	sound_file = vlc.MediaPlayer("Directory to electroinc chime")
 	sound_file.play()
 	time.sleep(2)
-	sound_file = vlc.MediaPlayer("/home/logan/Desktop/OpenDrive/disengage.mp3")
+	sound_file = vlc.MediaPlayer("directory to disengage.mp3")
 	sound_file.play()
 
 
 def Distracted_Sound():
-	sound_file = vlc.MediaPlayer("/home/logan/Desktop/OpenDrive/Distracted.mp3")
+	sound_file = vlc.MediaPlayer("Directory to Distracted.mp3")
 	sound_file.play()
 	time.sleep(1)
